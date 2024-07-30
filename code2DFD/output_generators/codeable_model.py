06d7e963686213bbf7aebfd4247e1a91e7a285a0
dominokos <dominik.jacob@tuhh.de>
2024-07-30 14:55:05 +0200

    make_codeable_model method implemented, still to be tested

--- code2DFD/output_generators/codeable_model.py
+++ code2DFD/output_generators/codeable_model.py
@@ -12,12 +12,18 @@ def filter_tagged_values(stereotypes):
         match = re.search(r"\w+ = \w*", stereotype)
         if match:
             key_value = stereotype.split(" = ")
+            stereotypes.pop(index)
             if key_value[0] == "Port":
                 tagged_values[key_value[0]] = int(key_value[1])
             else:
                 tagged_values[key_value[0]] = key_value[1]
-        stereotypes.pop(index)
     return tagged_values
+
+def remove_traceability(stereotypes):
+    output = list()
+    for index, (stereotype, traceability) in enumerate(stereotypes):
+        output[index] = stereotype
+    return output
 
 def make_codeable_model(model):
     """Entry function to creation of codeable models. Calls all necessary helper functions and outputs the codeable model"""
@@ -31,102 +37,54 @@ def make_codeable_model(model):
     
     for node in model.nodes.get_nodes():
         tagged_values = filter_tagged_values(node.stereotypes)
+        stereotypes = remove_traceability(node.stereotypes)
 
-        # Stereotypes
-        stereotypes = node.stereotypes
-    #     if "stereotype_instances" in microservices[m]:
-    #         for s in microservices[m]["stereotype_instances"]:
-    #             stereotypes.add(s)
+        node_type = str()
+        if stereotypes:
+            last = len(stereotypes)-1
+            stereotypes = str(list(stereotypes))
+            stereotypes = stereotypes.replace("'", "")
+            node_type = stereotypes[last]   
 
-    #         if stereotypes:
-    #             stereotypes = str(list(stereotypes))
-    #             stereotypes = stereotypes.replace("'", "")
+        name = str(node.name).replace("-", "_")
 
-    #     name = str(microservices[m]["servicename"]).replace("-", "_")
+        # Create entry
+        if stereotypes and tagged_values:
+            new_line = "\n" + name + " = CClass(" + node_type + ", \"" + str(node.name) + "\", stereotype_instances = " + str(stereotypes) + ", tagged_values = " + str(tagged_values) + ")"
+        elif stereotypes:
+            new_line = "\n" + name + " = CClass(" + node_type + ", \"" + str(node.name) + "\", stereotype_instances = " + str(stereotypes) + ")"
+        else:
+            new_line = "\n" + name + " = CClass(" + node_type + ", \"" + str(node.name) + "\")"
 
-    #     # Create entry
-    #     if stereotypes and tagged_values:
-    #         new_line = "\n" + name + " = CClass(service, \"" + str(microservices[m]["servicename"]) + "\", stereotype_instances = " + str(stereotypes) + ", tagged_values = " + str(tagged_values) + ")"
-    #     elif stereotypes:
-    #         new_line = "\n" + name + " = CClass(service, \"" + str(microservices[m]["servicename"]) + "\", stereotype_instances = " + str(stereotypes) + ")"
-    #     else:
-    #         new_line = "\n" + name + " = CClass(service, \"" + str(microservices[m]["servicename"]) + "\")"
-
-    #     file_content += new_line
-    #     last_node = name
+        file_content += new_line
+        last_node = name
 
 
-    # # External Components
-    # for e in external_components.keys():
-    #     # Tagged Values
-    #     tagged_values = dict()
-    #     if "tagged_values" in external_components[e]:
-    #         for t in external_components[e]["tagged_values"]:
-    #             if t[0] == "Port":
-    #                 tagged_values[t[0]] = int(t[1])
-    #             else:
-    #                 tagged_values[t[0]] = t[1]
+    for edge in model.edges:
+        tagged_values = filter_tagged_values(edge.stereotypes)
+        stereotypes = remove_traceability(edge.stereotypes)
+        
+        if stereotypes:
+            stereotypes = str(list(stereotypes))
+            stereotypes = stereotypes.replace("'", "")
 
-    #     # Stereotypes
-    #     stereotypes = set()
-    #     if "stereotype_instances" in external_components[e]:
-    #         for s in external_components[e]["stereotype_instances"]:
-    #             stereotypes.add(s)
-    #         if stereotypes:
-    #             stereotypes = str(list(stereotypes))
-    #             stereotypes = stereotypes.replace("'", "")
+        sender = str(edge.sender).replace("-", "_")
+        receiver = str(edge.receiver).replace("-", "_")
 
-    #     name = str(external_components[e]["name"]).replace("-", "_")
+        if stereotypes and tagged_values:
+            new_line = "\nadd_links({" + sender + ": " + receiver + "}, stereotype_instances = " + str(stereotypes) + ", tagged_values = " + str(tagged_values) + ")"
+        elif stereotypes:
+            new_line = "\nadd_links({" + sender + ": " + receiver + "}, stereotype_instances = " + str(stereotypes) + ")"
+        else:
+            new_line = "\nadd_links({" + sender + ": " + receiver + "})"
+        file_content += new_line
 
 
-    #     if stereotypes and tagged_values:
-    #         new_line = "\n" + name + " = CClass(external_component, \"" + str(external_components[e]["name"]) + "\", stereotype_instances = " + str(stereotypes) + ", tagged_values = " + str(tagged_values) + ")"
-    #     elif stereotypes:
-    #         new_line = "\n" + name + " = CClass(external_component, \"" + str(external_components[e]["name"]) + "\", stereotype_instances = " + str(stereotypes) + ")"
-    #     else:
-    #         new_line = "\n" + name + " = CClass(external_component, \"" + str(external_components[e]["name"]) + "\)"
-    #     file_content += new_line
+    file_content += footer(last_node)
 
-
-    # # Information Flows
-    # for i in information_flows.keys():
-    #     # Tagged Values
-    #     tagged_values = dict()
-    #     if "tagged_values" in information_flows[i]:
-    #         for t in information_flows[i]["tagged_values"]:
-    #             if t[0] == "Port":
-    #                 t[1] = int(t[1])
-    #             tagged_values[t[0]] = t[1]
-
-    #     # Stereotypes
-    #     stereotypes = set()
-    #     if "stereotype_instances" in information_flows[i]:
-    #         if type(information_flows[i]["stereotype_instances"]) == set or type(information_flows[i]["stereotype_instances"]) == list:
-    #             for s in information_flows[i]["stereotype_instances"]:
-    #                 stereotypes.add(s)
-    #         else:
-    #             stereotypes.add(information_flows[i]["stereotype_instances"])
-    #         if stereotypes:
-    #             stereotypes = str(list(stereotypes))
-    #             stereotypes = stereotypes.replace("'", "")
-
-    #     sender = str(information_flows[i]["sender"]).replace("-", "_")
-    #     receiver = str(information_flows[i]["receiver"]).replace("-", "_")
-
-    #     if stereotypes and tagged_values:
-    #         new_line = "\nadd_links({" + sender + ": " + receiver + "}, stereotype_instances = " + str(stereotypes) + ", tagged_values = " + str(tagged_values) + ")"
-    #     elif stereotypes:
-    #         new_line = "\nadd_links({" + sender + ": " + receiver + "}, stereotype_instances = " + str(stereotypes) + ")"
-    #     else:
-    #         new_line = "\nadd_links({" + sender + ": " + receiver + "})"
-    #     file_content += new_line
-
-
-    # file_content += footer(last_node)
-
-    # output_path = str()
-    # output_path = create_file(model_name, file_content)
-    # return file_content, output_path
+    output_path = str()
+    output_path = create_file(model_name, file_content)
+    return file_content, output_path
 
 
 def output_codeable_model(microservices, information_flows, external_components):
